a337e3d4c6add7fc2c05c416242f999d88cb3ab2
Advaith Suresh <56351354+Advaith123@users.noreply.github.com>
2021-05-14 08:44:41 +0530

    Update getting_data.py
    Added team names and first names

--- getting_data.py
+++ getting_data.py
@@ -5,6 +5,50 @@ import numpy as np
 
 index = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15']
 index_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+conditions = [
+    final_df['team'] == 1,
+    final_df['team'] == 2,
+    final_df['team'] == 3,
+    final_df['team'] == 4,
+    final_df['team'] == 5,
+    final_df['team'] == 6,
+    final_df['team'] == 7,
+    final_df['team'] == 8,
+    final_df['team'] == 9,
+    final_df['team'] == 10,
+    final_df['team'] == 11,
+    final_df['team'] == 12,
+    final_df['team'] == 13,
+    final_df['team'] == 14,
+    final_df['team'] == 15,
+    final_df['team'] == 16,
+    final_df['team'] == 17,
+    final_df['team'] == 18,
+    final_df['team'] == 19,
+    final_df['team'] == 20,
+]
+values = [
+    'ARS',
+    'AVL',
+    'BHA',
+    'BUR',
+    'CHE',
+    'CRY',
+    'EVE',
+    'FUL',
+    'LEI',
+    'LEE',
+    'LIV',
+    'MCI',
+    'MUN',
+    'NEW',
+    'SHU',
+    'SOU',
+    'TOT',
+    'WBA',
+    'WHU',
+    'WOL',
+]
 
 url = 'https://fantasy.premierleague.com/api/bootstrap-static/'
 r = requests.get(url)
@@ -18,12 +62,17 @@ elements_df = pd.DataFrame(json['elements'])
 elements_types_df = pd.DataFrame(json['element_types'])
 teams_df = pd.DataFrame(json['teams'])
 # print(teams_df.columns)
-
-# print(elements_df.head()) top 5 rows
-
+# print(elements_df.columns)
+team_list = []
+team_list = teams_df[['short_name']]
+# print(team_list)
+# print(team_list['Index'])
 # slim_elements_df = elements_df[['second_name','team','element_type','selected_by_percent','now_cost','minutes','transfers_in','value_season','total_points']]
-slim_elements_df = elements_df[['id', 'second_name', 'element_type', 'points_per_game', 'transfers_in', 'selected_by_percent', 'team_code', 'transfers_out', 'goals_scored', 'assists', 'clean_sheets', 'ict_index', 'chance_of_playing_this_round', 'minutes', 'form', 'saves']]
+slim_elements_df = elements_df[['id','first_name', 'second_name', 'element_type', 'points_per_game', 'transfers_in', 'selected_by_percent', 'team', 'transfers_out', 'goals_scored', 'assists', 'clean_sheets', 'ict_index', 'chance_of_playing_this_round', 'minutes', 'form', 'saves']]
 sorted_by_form_elements_df = slim_elements_df.sort_values('form', ascending=False).head(20)
+# for index, row in slim_elements_df.iterrows():
+#     for x in team_list:   ITERATING THROUGH THE DF
+#         if row['team'] - 1 == team_list['Index']
 # sorted_by_ict_elements_df = slim_elements_df.sort_values('ict_index', ascending=False).head(20)
 # sorted_by_points_per_game_elements_df = slim_elements_df.sort_values('points_per_game', ascending=False).head(20)
 # sorted_by_minutes_elements_df = slim_elements_df.sort_values('minutes', ascending=False).head(20)
@@ -38,6 +87,9 @@ sorted_by_form_elements_df = slim_elements_df.sort_values('form', ascending=Fals
 def sorting(df):
     df = df.sort_values('form', ascending=False)
     return df
+def team_name(row):
+    if row['team'] == 17:
+        return "TOT"
 
 gk_df = slim_elements_df.loc[slim_elements_df.element_type == 1]
 # print(gk_df.loc[7])
@@ -62,6 +114,12 @@ test_df = final_df
 test_df['Index'] = index
 # print(test_df.set_index(test_df.Index))
 final_df = test_df.set_index(test_df.Index)
+# final_df['team_name'] = final_df.apply(team_name())
+
+
+final_df['team_name'] = np.select(conditions, values) #elegant solution
+
+# print(final_df[['first_name', 'second_name', 'team', 'team_name']])
 # print(test_df)
 # print(test_df.element_type)
 # print(elements_df)
